dd9538ec323fe47acac533e553de84b4e699a4b8
Nathan  Visak <nathan.visak@gmail.com>
2018-09-03 19:14:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,10 +11,10 @@ class App < Sinatra::Base
     square.to_s
   end
 
-  get '/say/:number/:phrase' do 
-    params[:number].to_i.times do 
+  get '/say/:number/:phrase' do
+    params[:number].to_i.times do
       params[:phrase]
     end 
-  end 
+  end
 
 end
